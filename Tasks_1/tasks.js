b1b2c11f0ade4d33942fe2fe6af2547cd974842b
Saad <66124515+sadsultan@users.noreply.github.com>
2023-07-28 18:49:12 +0500

    Completed extra task

--- Tasks_1/tasks.js
+++ Tasks_1/tasks.js
@@ -30,3 +30,29 @@ console.log(myObject.sumProperty);
 
 // Task 10
 console.log(myObject.firstProperty[1]);
+
+// Extra Tasks
+// Task 1
+let numberVariable = 0;
+numberVariable += 3;
+console.log(numberVariable);
+
+// Task 2
+// Both snippets are the exact same with no differences in the code or output.
+
+// Task 3
+const expression1 = 100 % 50; // 0
+const expression2 = 100 / 50; // 2
+const expression3 = expression1 < expression2; // true
+const expression4 = expression3 && 300 + 5 === 305 ; // true
+const expression5 = !expression4; // false
+const unitedExpression = (!(100%5 < 100/50) && 300 + 5 === 305); // true
+
+console.log(expression5);
+console.log(unitedExpression);
+
+// Task 4
+// It evaluates to true
+
+// Task 5
+// Evaluates to true
---
+++ Tasks_1/tempCodeRunnerFile.js
@@ -0,0 +1,13 @@
+console.log(myObject);
+
+// // Task 9
+// console.log(myObject.sumProperty);
+
+// // Task 10
+// console.log(myObject.firstProperty[1]);
+
+// // Extra Tasks
+// // Task 1
+// let numberVariable = 0;
+// numberVariable += 3;
+// console.log(numberVariable);
